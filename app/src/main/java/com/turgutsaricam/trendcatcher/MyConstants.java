58bf963ff3c41f607ea115af3893cd2b4e6a84ce
Turgut <turgutsaricam@users.noreply.github.com>
2015-03-05 21:40:54 +0200

    Update MyConstants.java

--- app/src/main/java/com/turgutsaricam/trendcatcher/MyConstants.java
+++ app/src/main/java/com/turgutsaricam/trendcatcher/MyConstants.java
@@ -7,11 +7,11 @@ package com.turgutsaricam.trendcatcher;
 public class MyConstants {
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
-    public static final String TWITTER_KEY = "FXskRh91YQ6QkKKayqN32J8Cm";
-    public static final String TWITTER_SECRET = "FQWpdIs7nibsZ5TuRszw0uRVL6CrMPnNumCTCcLmSqjKZLgVXh";
+    public static final String TWITTER_KEY = "*";
+    public static final String TWITTER_SECRET = "*";
 
-    public static final String CONSUMER_KEY = "PenvyWzisM0UPAW8lLQ7wSSL3";
-    public static final String CONSUMER_SECRET = "SrF0Ns9JyKT3kHm3K1Zt68iX4PqGXA8siQpw4V5NONRRyXCivT";
-    public static final String ACCESS_TOKEN = "36305699-r5FpKozr9EifChYcBeOfMGyFWu7mH2iYx1cKnoenz";
-    public static final String ACCESS_TOKEN_SECRET = "vLl0ZYMFp6LhBmKNqtotFjDgkcKaxFIpySBZ14IBYCuzC";
+    public static final String CONSUMER_KEY = "*";
+    public static final String CONSUMER_SECRET = "*";
+    public static final String ACCESS_TOKEN = "*";
+    public static final String ACCESS_TOKEN_SECRET = "*";
 }
